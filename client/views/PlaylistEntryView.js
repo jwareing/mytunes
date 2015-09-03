0fe4689a5f4a897453d250948783b573553b1788
jwareing <jwareing89@gmail.com>
2015-09-03 14:48:02 -0700

    Added shell files for PlaylistEntryView and PlaylistView.

---
+++ client/views/PlaylistEntryView.js
@@ -0,0 +1,30 @@
+// LibraryEntryView.js - Defines a backbone view class for the entries that will appear within the library views. These will be inserted using the "subview" pattern.
+var PlaylistEntryView = Backbone.View.extend({
+
+  initialize: function() {
+    this.model.on('change', this.render, this);
+  },
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td class="title"><%= title %></td> \
+                        <td><%= playCount %></td><td><button id="removeFromPlaylist">Remove</button></td>'),
+
+  events: {
+    'click .title': function() {
+      //POINT OF INTEREST!!!!
+      console.dir(this);
+      // this.model.play();
+      this.model.enqueue();
+    },
+
+    'click button' : function() {
+      this.model.playlistAdd();
+    }
+  },
+
+  render: function(){
+    return this.$el.html(this.template(this.model.attributes));
+  }
+
+});
